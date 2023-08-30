650db419979f9c1d11a6413d56926d853ecbf0e6
pedro-bs <143233005+pedro-bs@users.noreply.github.com>
2023-08-30 15:36:01 -0300

    Update App.js

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -18,3 +18,82 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+--------------------------------------------
+
+   import {
+  Alert,
+  FlatList,
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+
+import AddTodo from './components/AddTodo';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '1' },
+    { id: 2, text: '2' },
+    { id: 3, text: '3' },
+    { id: 4, text: '4' },
+    { id: 5, text: '5' },
+  ]);
+
+  const pressHandler = id => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== id);
+    });
+  };
+
+  const submitHandler = text => {
+    if (text.length > 3) {
+      setTodos(prevTodos => {
+        return [...prevTodos, { text, id: todos.length + 1 }]; // UUID
+      });
+    } else {
+      Alert.alert(
+        'Erro!',
+        'As tarefas precisam ter no mínimo 3 caracteres.',
+        [{ text: 'Entendi', onPress: () => console.log('Alerta fechado.') }]
+      );
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+        console.log('Saí teclado!');
+      }}
+    >
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              keyExtractor={todo => todo.id}
+              renderItem={({ item }) => (
+                <TodoItem item={item} onPress={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
