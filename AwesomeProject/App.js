0c6426cd371d6411808377dc09a3a01608b7497d
pedro-bs <143233005+pedro-bs@users.noreply.github.com>
2023-08-30 17:00:22 -0300

    cometarios add

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,3 +1,4 @@
+/*
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -19,8 +20,11 @@ const styles = StyleSheet.create({
   },
 });
 --------------------------------------------
+*/
+/*
 
-   import {
+*/
+import {
   Alert,
   FlatList,
   Keyboard,
